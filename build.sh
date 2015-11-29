8adc63ff314a9646a9862169169b0c4a369c1135
Palocz Istvan <istvan@palocz.hu>
2015-11-29 11:35:02 +0100

    Initial commit

---
+++ build.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+docker build -t tanarurkerem/sonar-php:5.2 5.2
+docker build -t tanarurkerem/sonar-php:latest 5.2
+docker build -t tanarurkerem/sonar-php:4.5.6 4.5.6
+docker build -t tanarurkerem/sonar-php:lts 4.5.6
